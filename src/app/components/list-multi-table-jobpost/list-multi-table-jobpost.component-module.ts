903863f66a5a8ec312ce20866cc41d2948ce7900
Natalia Klekocińska <klekocinskan@gmail.com>
2023-01-12 18:23:30 +0100

    list-multi-table-jobpost

---
+++ src/app/components/list-multi-table-jobpost/list-multi-table-jobpost.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ListMultiTableJobpostComponent } from './list-multi-table-jobpost.component';
+
+@NgModule({
+  imports: [],
+  declarations: [ListMultiTableJobpostComponent],
+  providers: [],
+  exports: [ListMultiTableJobpostComponent]
+})
+export class ListMultiTableJobpostComponentModule {
+}
---
+++ src/app/components/list-multi-table-jobpost/list-multi-table-jobpost.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-list-multi-table-jobpost',
+  styleUrls: ['./list-multi-table-jobpost.component.scss'],
+  templateUrl: './list-multi-table-jobpost.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ListMultiTableJobpostComponent {
+}
